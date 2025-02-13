5bb16fbb769d666355487ccff9e3ec22ed9becd7
Alexclansy24 <123365844+Alexclansy24@users.noreply.github.com>
2025-02-13 17:08:08 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = "https://food-delivery-backend-ql3j.onrender.com"
+export const url = "https://food-delivery-backend-ql3j.onrender.com";
